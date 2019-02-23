c8aa6d603e31e48e1f3e3fc8c0ef5addd8582e7f
sirchris <florkowski.krzysztof@gmail.com>
2019-02-23 13:00:52 +0100

    List Rendering

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ var app = new Vue({
         image: './img/vmSocks-blue-onWhite.jpg',
         link:
             'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-        inventory: 9
+        inventory: 9,
+        details: ['80% cotton', '20% polyester', 'Gender-neutral'],
+        variants: [
+            { variantId: 2234, variantColor: 'blue' },
+            { variantId: 2235, variantColor: 'green' }
+        ]
     }
 });
